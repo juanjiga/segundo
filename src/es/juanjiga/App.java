87f4e54246e0c301e14570fa8d2092cbb49b4ae9
juanjiga <juanjiga71@hotmail.es>
2023-12-05 23:24:53 +0100

    primer commit de segundo

---
+++ src/es/juanjiga/App.java
@@ -0,0 +1,11 @@
+package es.juanjiga;
+
+import es.juanjiga.ventana.Marco;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Probando VS Code y Java");
+        Marco mimarco = new Marco();
+        mimarco.setVisible(true);
+    }
+}
---
+++ src/es/juanjiga/ventana/Marco.java
@@ -0,0 +1,15 @@
+package es.juanjiga.ventana;
+
+import javax.swing.JFrame;
+
+public class Marco extends JFrame{
+        public Marco(){
+        //setSize(300, 300);
+        //etLocation(500, 500);
+        setBounds(500, 500, 350, 350);
+        setResizable(true);
+        setTitle("Ventanass");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }    
+
+}
