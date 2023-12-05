514119724b3a7acb089e6f632d27ff9ca7927977
juanjiga <juanjiga71@hotmail.es>
2023-12-05 23:34:15 +0100

    segundoCommit de segundo

--- src/es/juanjiga/App.java
+++ src/es/juanjiga/App.java
@@ -5,6 +5,7 @@ import es.juanjiga.ventana.Marco;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Probando VS Code y Java");
+        System.out.println(".....tambien probando Git");
         Marco mimarco = new Marco();
         mimarco.setVisible(true);
     }
